d7e5c89bbf500a0e87669614422063d981f5ce77
Si Jobling <si@jobling.com>
2026-01-26 21:05:14 +0000

    Refactor CSS build scripts and update PostCSS configuration for improved development and production workflows

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   plugins: [
     require('postcss-import')({
@@ -8,6 +10,6 @@ module.exports = {
       ]
     }),
     require('autoprefixer'),
-    require('cssnano')
+    ...(isProd ? [require('cssnano')] : [])
   ]
 }
